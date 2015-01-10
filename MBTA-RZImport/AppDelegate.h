0043fcc834cbd2d9a2ef7335bb79e529eabc9343
Steve Caine <scaine@mac.com>
2015-01-10 13:50:17 -0500

    Entab, pull up open braces.

--- MBTA-RZImport/AppDelegate.h
+++ MBTA-RZImport/AppDelegate.h
@@ -1,9 +1,9 @@
 //
-//  AppDelegate.h
-//  MBTA-RZImport
+//	AppDelegate.h
+//	MBTA-RZImport
 //
-//  Created by Steve Caine on 01/10/15.
-//  Copyright (c) 2015 Steve Caine. All rights reserved.
+//	Created by Steve Caine on 01/10/15.
+//	Copyright (c) 2015 Steve Caine. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- MBTA-RZImport/MasterViewController.h
+++ MBTA-RZImport/MasterViewController.h
@@ -1,9 +1,9 @@
 //
-//  MasterViewController.h
-//  MBTA-RZImport
+//	MasterViewController.h
+//	MBTA-RZImport
 //
-//  Created by Steve Caine on 01/10/15.
-//  Copyright (c) 2015 Steve Caine. All rights reserved.
+//	Created by Steve Caine on 01/10/15.
+//	Copyright (c) 2015 Steve Caine. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
