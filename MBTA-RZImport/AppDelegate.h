7f1903daf4e6bdb3f9cdc3bf144d0de15914647d
Steve Caine <scaine@mac.com>
2015-01-10 13:46:47 -0500

    Initial Commit

---
+++ MBTA-RZImport/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MBTA-RZImport
+//
+//  Created by Steve Caine on 01/10/15.
+//  Copyright (c) 2015 Steve Caine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MBTA-RZImport/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  MBTA-RZImport
+//
+//  Created by Steve Caine on 01/10/15.
+//  Copyright (c) 2015 Steve Caine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
